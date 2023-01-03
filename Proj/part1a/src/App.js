2ecf1f64a03b9fdd94f1f6d7eb86616321358c70
Siddharth <sidkaduskar.com>
2023-01-03 23:31:24 +0530

    part1a was fixed

--- Proj/part1a/src/App.js
+++ Proj/part1a/src/App.js
@@ -2,9 +2,6 @@
 // import './App.css';
 
 import Content from "./components/content";
-import P1 from "./components/content/part1";
-import P2 from "./components/content/part2";
-import P3 from "./components/content/part3";
 import Header from "./components/header";
 import Total from "./components/total";
 
@@ -22,21 +19,13 @@ const App = () => {
     <div>
       {/* passing an object */}
       <Header course={Course} />
-      {/* passing array of objects */}
-      <P1 parts={parts}/>
-      <Content parts='Fundamentals of React'
+        {/* passing props  */}
+       <Content parts='Fundamentals of React'
         exercises1={10}
         part2='Using props to pass data'
         exercises2={7}
         part3='State of a component'
         exercises3={14} />
-        {/* passing props */}
-        {/* <Content parts='Fundamentals of React'
-        exercises1={10}
-        part2='Using props to pass data'
-        exercises2={7}
-        part3='State of a component'
-        exercises3={14} /> */}
       <Total exercises1={10}
         exercises2={7}
         exercises3={14} />
