1ea79f85a7f8065248fc40303699c15ffd00799d
Siddharth <sidkaduskar.com>
2023-01-04 13:41:34 +0530

    header.js and Part_1_c was edited

--- Proj/part1a/src/components/header.js
+++ Proj/part1a/src/components/header.js
@@ -8,3 +8,17 @@ const Header = (course) => {
   }
 
   export default Header;
+
+// the following works too, if we pass any obejct as props, the system will work just fine,
+//thats the way it is IG.
+
+  // const Header = (props) => {
+  //   return (
+  //     <>
+  //      <h1>{props.course}</h1>
+  //      {/* <h1>{props.course}</h1> */}
+  //     </>
+  //   )
+  // }
+
+  // export default Header;
