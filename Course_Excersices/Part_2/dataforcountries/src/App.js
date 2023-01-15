c2241841094fc9581158c8172ab50c88113000e1
Siddharth <sidkaduskar.com>
2023-01-15 13:40:18 +0530

    initial Commit

---
+++ Course_Excersices/Part_2/dataforcountries/src/App.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+
+function App() {
+  const [notes, setNotes] = useState([])
+  useEffect(() => {
+    axios.get('https://restcountries.com/v3.1/all')
+      .then(request => setNotes(request.data))
+  }, [])
+
+  const [temp, setTemp] = useState('')
+  const [search, setsearch] = useState([])
+  const [TempS, setTempS] = useState([])
+
+  let watchsearch = (event) => {
+    setTemp(event.target.value)
+  }
+
+  const submitquery = (event) => {
+    setsearch([])
+    setTempS([])
+    notes.map((ext) => {
+      if (ext.name.common.includes(temp) && !search.includes(ext)) {
+        setTempS(search)
+        TempS.push(ext)
+        setsearch(TempS)
+      }
+    })
+    event.preventDefault()
+  }
+  console.log("Yeh hai", search)
+
+  // const submitquery = (temp, event) => {
+  //   setDisplay(notes.map(ext => ext.name.common === event))
+  //   console.log("Yeh hai", display)
+  //   temp.preventDefault()
+  // }
+  console.log("search =", search)
+  return (
+    <>
+      <form onSubmit={submitquery}>
+        <div>
+          name: <input onChange={watchsearch} />
+        </div>
+        <div>
+          <button type="Submit">add</button>
+        </div>
+      </form>s
+    </>
+
+  );
+}
+
+export default App;
---
+++ Course_Excersices/Part_2/dataforcountries/src/components/display.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function display({obj}) {
+    // if () {
+    //     return (
+    //         <div>display</div>
+    //     )
+    // }
+
+    // else if () {
+
+
+    //     return (
+    //         <div>display</div>
+    //     )
+    // }
+}
+
+export default display
