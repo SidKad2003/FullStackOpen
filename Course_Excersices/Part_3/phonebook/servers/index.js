7952fa2881e32124d2faf2846b3c43d9c5d40073
Siddharth <sidkaduskar.com>
2023-01-19 21:16:14 +0530

    initial commit

---
+++ Course_Excersices/Part_3/phonebook/servers/index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+
+let notes = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+let now = new Date()
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for 2 people</p><p> ${now}</p>`)
+}
+)
+// console.log(getDate())
+
+app.get('/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/notes/:id', (request, response) => {
+    const id = request.params.id
+    const note = notes.find(note => note.id === id)
+    response.json(note)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
---
+++ Course_Excersices/Part_3/phonebook/src/App.js
@@ -0,0 +1,65 @@
+import './App.css';
+import { useState } from 'react';
+import Add from './components/Add';
+import Setfilter from './components/Setfilter';
+import Display from './components/Display';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', contact: 65654446 }
+  ])
+  useEffect(()=>{
+    axios.get('http://localhost:3001/persons')
+    .then(response => setPersons(response.data))
+  }, [])
+    const [newName, setNewName] = useState('')
+    const [newcontact, setNewcontact] = useState('')
+    const [show, setShow] = useState([{name: 'Arto Hellas', contact: 65654446}])
+    const [newfilter, setNewfilter] = useState('')
+  const changing = (event) => {
+    setNewName((event.target.value))
+  }
+  const changingfilter = (event) => {
+    setNewfilter((event.target.value))
+  }
+  const changingcontact = (event) => {
+    setNewcontact((event.target.value))
+  }
+  const addobj = {
+    name: newName,
+    contact: newcontact
+    // date: new Date().toISOString(),
+    // important: Math.random() < 0.5,
+    // id: notes.length + 1,
+  }
+  const filtering = () => {
+    setShow(persons.filter(persons => persons.name === newfilter))
+    console.log(show)
+   }
+   
+   const addnew = (event) => {
+    console.log(newName)
+    setPersons(persons.concat(addobj))
+    console.log(persons)
+    alert(`${addobj.name} was Added with contact No. as ${addobj.contact}`)
+    axios.post('http://localhost:3001/persons', addobj)
+    event.preventDefault()
+  }
+  
+  return (
+    <div>
+      <h2>Add a New</h2>
+      <Add addnew={addnew} changing={changing} changingcontact={changingcontact}/>
+      <h3>Filter</h3>
+      <Setfilter changingfilter={changingfilter} filtering={filtering}/>
+      <Display persons={persons} show={show}/>
+      
+
+    </div>
+    
+  )
+}
+
+export default App
